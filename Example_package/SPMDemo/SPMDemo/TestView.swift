b9d9f3c486ef282b4dbcd34b6d0d7ef2db365d00
snail-z <40460770@qq.com>
2025-07-08 20:16:04 +0800

    spm

---
+++ Example_package/SPMDemo/SPMDemo/TestView.swift
@@ -0,0 +1,24 @@
+//
+//  TestView.swift
+//  SPMDemo
+//
+//  Created by zhanghao on 2025/7/8.
+//
+
+import UIKit
+import PopupKit
+
+class TestView: UIView {
+    
+}
+
+extension TestView: PopupPresentable {
+    
+    func preferredPopupSize(in container: UIView) -> CGSize {
+        let maxW = UIScreen.main.bounds.width - 100
+//        let size = systemLayoutSizeFitting(
+//            CGSize(width: maxW, height: 300)
+//        )
+        return CGSize(width: maxW, height: 400)
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "PopupKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "PopupKit",  // 对外 `import PopupKit`
+            targets: ["PopupKit"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "PopupKit",
+            dependencies: [],
+            path: "Sources/PopupKit"
+        ),
+        .testTarget(
+            name: "PopupKitTests",
+            dependencies: ["PopupKit"],
+            path: "Tests/PopupKitTests"
+        )
+    ]
+)
