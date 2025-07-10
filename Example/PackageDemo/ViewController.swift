4fce9c514bf97b7efe4b47c8a29a72a0617bed5f
snail-z <40460770@qq.com>
2025-07-10 11:38:04 +0800

    update

---
+++ Example/PackageDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PackageDemo
+//
+//  Created by zhanghao on 2025/7/10.
+//
+
+import UIKit
+import DawnTransition
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        self.dawn.isModalEnabled = true
+    }
+
+
+}
+
